ca1cf77ce596ba120ad00e6ac28918b16acb25d7
Stephanie Snyder <snyders8992@my.sduhsd.net>
2019-12-19 12:41:51 -0800

    Iteration

--- ifstmnt.c
+++ ifstmnt.c
@@ -2,15 +2,65 @@
 
 int main()
 {
-	int div = 3;
-	int i;
-	for (i=1; i<=100; i++)
-	{
-		if (i%div == 0)
-		{
-		printf("%d\n", i);
-		}
+int a = 0;
+int b = 2;
+  // if statement to test is a is equal to 0
+  if (a == b)
+  {
+    printf("a is b\n");
+  }
+  else
+  {
+    printf("a is not b\n");
+  }
 
-	}
-return 0;
+if (a != b)
+  {
+    printf("a is not b\n");
+  }
+  else
+  {
+    printf("a is b\n");
+  }
+if (a > b)
+  {
+    printf("a is greater than b\n");
+  }
+  else
+  {
+    printf("a is less than or equal to b\n");
+  }
+if (a >= b)
+  {
+    printf("a is greater than or equal to b\n");
+  }
+  else
+  {
+    printf("a is less than b\n");
+  }
+if (a == 0 && b == 0)
+  {
+    printf("a is equal to 0 and b is equal to 0\n");
+  }
+  else
+  {
+    printf("a is not equal to 0 or b is not equal to 0 or a and b are not equal to 0\n");
+  }
+if (a == 0 || b == 0)
+  {
+    printf("a or b is equal to 0\n");
+  }
+  else
+  {
+    printf("a and b are not equal to 0\n");
+  }
+if (!(a == 0))
+  {
+    printf("a is not equal to\n");
+  }
+  else
+  {
+    printf("a is equal to 0\n");
+  }
+
 }
