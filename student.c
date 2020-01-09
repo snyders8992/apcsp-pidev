f432ccdfd012c908563e7d8404e9e0736abcff17
Stephanie Snyder <snyders8992@my.sduhsd.net>
2020-01-09 12:19:36 -0800

    Student

---
+++ student.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student
+{
+  char first[50];
+  char last[50];
+  int age;
+  int studentid;
+};
+
+void printStudent(struct Student* student)
+{
+  printf("Name: %s %s\n", student->first, student->last);
+  printf("Age: %d\n", student->age);
+  printf("StudentID: %d\n", student->studentid);
+}
+
+int main()
+{
+
+  char input[256];
+  struct Student student1;
+  struct Student student2;
+
+  strcpy(student1.first, "Stephanie");
+  strcpy(student1.last, "Snyder");
+  student1.age = 17;
+  student1.studentid = 908992;
+
+  printf("What is your first name?\n");
+  fgets(input, 256, stdin);
+  sscanf(input, "%s", student2.first);
+
+  printf("What is your last name?\n");
+  fgets(input, 256, stdin);
+  sscanf(input, "%s", student2.last);
+
+  printf("What is your age?\n");
+  fgets(input, 256, stdin);
+  sscanf(input, "%d", &student2.age);
+
+  printf("What is your StudentID?\n");
+  fgets(input, 256, stdin);
+  sscanf(input, "%d", &student2.studentid);
+
+  printStudent(&student1);
+  printStudent(&student2);
+
+
+  return 0;
+}
