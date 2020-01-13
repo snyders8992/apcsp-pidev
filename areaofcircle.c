cb969423263067e7b6295a4614a0515d25f1f40e
Stephanie Snyder <snyders8992@my.sduhsd.net>
2020-01-13 12:11:59 -0800

    done

---
+++ areaofcircle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int X =0;
+float a = 0;
+
+int areaOfCircle (float a)
+{
+
+int X = ((a*a)*3.14);
+
+printf("%d\n",X);
+
+}
+
+ for (float a = 3.5; a < 12.5; a++){
+
+areaOfCircle(a);
+
+}
+
+}
